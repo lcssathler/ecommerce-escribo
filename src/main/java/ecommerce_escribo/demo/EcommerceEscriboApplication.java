ef32954c7fa44418838d4b042b3387bedc1df20d
lcssathler <lcssathler@gmail.com>
2025-10-17 16:27:53 -0300

    first commit

---
+++ src/main/java/ecommerce_escribo/demo/EcommerceEscriboApplication.java
@@ -0,0 +1,13 @@
+package ecommerce_escribo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceEscriboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceEscriboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommerce_escribo/demo/EcommerceEscriboApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce_escribo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceEscriboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
